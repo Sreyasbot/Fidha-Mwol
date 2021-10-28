f543192e68747ba7c5ee655b320d1d2c40800744
Sreyas <92863568+Sreyasbot@users.noreply.github.com>
2021-10-28 16:09:01 +0530

    Update hypama.js

--- plugins/hypama.js
+++ plugins/hypama.js
@@ -1,8 +1,8 @@
-/* Copyright (C) 2021 AMALSER.
+/* Copyright (C) 2021 ꪶɪᴛs-ᴍᴇ ͢⁨ғᴀᴋᴇ sʀᴇʏᴀsꫂ»
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Amalser - Amal
-Wa.me/+919895828468
+Wa.me/+918547393489
 */
 
 const Neotro = require('../events');
